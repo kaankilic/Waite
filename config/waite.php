2dfa0612a47dd7fb1face4487216a9e19f30c771
Kaan Kılıç <kaan@kaankilic.com>
2017-05-13 17:56:23 +0300

    zip function implemented

--- config/waite.php
+++ config/waite.php
@@ -4,6 +4,9 @@ return [
 	"canUpdateViews" 	=> true,
 	"config_folder" 	=> app_path()."/config",
 	"ignore_files"		=> [],
-	"ignore_folders" 	=> []
+	"ignore_folders" 	=> [],
+	"repository"		=> ":owner/:repo",
+	"version_format"	=> "v[0-9]*.[0-9]*.[0-9]*",
+	"storage"			=> "uploads"
 ];
 ?>
--- src/Libraries/Waite.php
+++ src/Libraries/Waite.php
@@ -1,29 +1,37 @@
 <?php 
 namespace Kaankilic\Waite\Libraries;
+use Illuminate\Support\Facades\Storage;
 class Waite{
-	const GITHUB_TAGS_ENDPONT="/repos/:owner/:repo/tags";
+	const GITHUB_TAGS_ENDPONT="/repos/:owner/:repo/releases/latest";
 	protected $currentVersion;
-	protected $versionList = array();
+	public $latestVersion;
+	public $latestPackage;
 	protected $isSuccessful;
 	protected checkForVersions(){
 		$URL = "http://github.com";
 	}
 	public function getCurrentVersion(){
-		return $this->currentVersion();
+		return Storage::disk("upload")->get("VERSION.md");
 	}
 	public function setCurrentVersion($currentVersion){
 		$this->currentVersion = $currentVersion;
 	}
-	public function findNextVersion(){
-		foreach ($versionList as $version) {
-			
+	public function getLatestVersion(){
+		$client = new \GuzzleHttp\Client();	
+		$res = $client->request('GET', "https://api.github.com/".self::GITHUB_TAGS_ENDPONT);
+		if ($res->getStatusCode()==200) {
+			$this->latestVersion = $res->getBody->tag_name;
+			$this->latestPackage = $res->getBody->zipball_url;
 		}
 	}
-	private function upload(){
-
+	private function hasVersion(){
+		return version_compare($this->getCurrentVersion(),$this->getLatestVersion(),"<");
 	}
 	private function update(){
-
+		if ($this->hasVersion()) {
+			Storage::disk("upload")->put($this->getLatestPackage,"latest_version.zip");
+			Zipper::make('latest_version.zip')->extractTo('/', array('vendor','config'), Zipper::BLACKLIST);
+		}
 	}
 }
 ?>
