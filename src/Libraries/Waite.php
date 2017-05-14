a71fd78523680cb597c7261e403a83d6424860cb
Kaan Kilic <bl4cksta@gmail.com>
2017-05-15 00:10:39 +0300

    minor bug fixes

--- src/Libraries/Waite.php
+++ src/Libraries/Waite.php
@@ -2,35 +2,38 @@
 namespace Kaankilic\Waite\Libraries;
 use Illuminate\Support\Facades\Storage;
 class Waite{
-	const GITHUB_TAGS_ENDPONT="/repos/:owner/:repo/releases/latest";
-	protected $currentVersion;
-	public $latestVersion;
-	public $latestPackage;
+	const GITHUB_TAGS_ENDPONT="/repos/kaankilic/disket/releases/latest";
+	const GITHUB_API_ENDPONT="https://api.github.com";
+
+	protected static $latestVersion;
+	protected static $latestPackage;
+
 	protected $isSuccessful;
-	protected  function checkForVersions(){
-		$URL = "http://github.com";
+
+	private static function getCurrentVersion(){
+		$version = Storage::disk("local")->get("VERSION.md");
+		return $version;
 	}
-	public function getCurrentVersion(){
-		return Storage::disk("upload")->get("VERSION.md");
+	private static function getEndpoint(){
+		return self::GITHUB_API_ENDPONT.self::GITHUB_TAGS_ENDPONT;
 	}
-	public function setCurrentVersion($currentVersion){
-		self::$currentVersion = $currentVersion;
-	}
-	public function getLatestVersion(){
+	private static function getLatestVersion(){
 		$client = new \GuzzleHttp\Client();	
-		$res = $client->request('GET', "https://api.github.com/".self::GITHUB_TAGS_ENDPONT);
+		$res = $client->request('GET', self::getEndpoint(),['headers'=>['Accept'=>'application/vnd.github.v3+json']]);
 		if ($res->getStatusCode()==200) {
-			self::$latestVersion = $res->getBody->tag_name;
-			self::$latestPackage = $res->getBody->zipball_url;
+			self::$latestVersion = json_decode($res->getBody())->tag_name;
+			self::$latestPackage = json_decode($res->getBody())->zipball_url;
 		}
 	}
-	private function hasVersion(){
+	private static function hasVersion(){
 		return version_compare(self::getCurrentVersion(),self::getLatestVersion(),"<");
 	}
 	public static function update(){
 		if (self::hasVersion()) {
-			Storage::disk("upload")->put(self::getLatestPackage,"latest_version.zip");
-			Zipper::make('latest_version.zip')->extractTo('/', array('vendor','config'), Zipper::BLACKLIST);
+			Storage::disk("local")->put(self::getLatestPackage,"latest_version.zip");
+			Zipper::make('latest_version.zip')->extractTo('/', array('vendor','config','storage','public'), Zipper::BLACKLIST);
+		}else{
+			echo 'already updated';
 		}
 	}
 }
